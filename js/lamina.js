68fcd3f69a5e148c9f4fccb436a1edef0b4bacbb
juliaChaparro <julia.ferreira@icomp.ufam.edu.br>
2026-05-25 14:45:24 -0400

    desenho de rastro feito e remover depois de um tempo tbm

--- js/lamina.js
+++ js/lamina.js
@@ -9,6 +9,10 @@ class Lamina {
         document.addEventListener("mousemove", (evento) =>{
             this.atualizarPosicaoMouse(evento);
             
+             if(this.estaCortando){
+                this.desenharRastro();
+            }
+            
         });
 
         document.addEventListener("mousedown", (evento) => {
@@ -34,4 +38,48 @@ class Lamina {
         console.log(`Posicao Anterior X: ${this.posicaoAnteriorX}`);
         console.log(`Posiçao Anterior Y:${this.posicaoAnteriorY}`);
     }
+
+
+    calcularDistanciaPontos(evento){
+        const dx = this.posicaoAtualX - this.posicaoAnteriorX;
+        const dy = this.posicaoAtualY - this.posicaoAnteriorY;
+        return Math.hypot(dx, dy);  // Retorna a distância em linha reta
+    }
+
+    calcularAngulo(){
+        const dx = this.posicaoAtualX - this.posicaoAnteriorX;
+        const dy = this.posicaoAtualY - this.posicaoAnteriorY;
+        return Math.atan2(dy, dx); //Retorna o angulo 
+    }
+
+    desenharRastro(){
+        const linha = document.createElement("div");
+
+        linha.classList.add("corte");
+        const distancia = this.calcularDistanciaPontos();
+
+        const angulo = this.calcularAngulo();
+
+        // posição inicial
+        linha.style.left = this.posicaoAnteriorX + "px";
+
+        linha.style.top = this.posicaoAnteriorY + "px";
+
+        // tamanho da linha
+        linha.style.width = distancia + "px";
+
+        // rotação
+        linha.style.transform = `rotate(${angulo}rad)`;
+
+        document.body.appendChild(linha);
+
+        // remove depois de um tempo
+        setTimeout(() => {
+            linha.remove();
+        }, 300);
+    }
+
 }
+
+const mouse = new Lamina();
+
